2d682dd39d608debeb13a60ae8661f796e91ed90
lompdoctor <pleclair@protonmail.com>
2023-05-15 18:36:49 -0500

    Deply to Fly.io

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // MongoDB configuration
 const MONGO_URI = 'mongodb+srv://fullstack:fs0pen@cluster0.00quc3x.mongodb.net/?retryWrites=true&w=majority';
@@ -29,6 +29,7 @@ const Configuration = mongoose.model('Configuration', configurationSchema);
 
 // Middleware
 app.use(bodyParser.json());
+app.use(express.static('build'))
 
 // Save configuration endpoint
 app.post('/api/configurations', (req, res) => {
